807131ac910dfc59cadc2eb4d48fd53e9c027568
kuni <tetsuji.kunitake@jp.ricoh.com>
2023-02-10 22:42:01 +0900

    2nd commit

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from apps import users, rooms, bookings # import your app modules here
 
 app= MultiApp()
 
+app.add_app("bookings", bookings.app)
 app.add_app("users", users.app)
 app.add_app("rooms", rooms.app)
-app.add_app("bookings", bookings.app)
 
 app.run()
